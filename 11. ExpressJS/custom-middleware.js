c18cd94c9b669fdf765fcc3776fef61d8a86b97a
kaushik0010 <paykolikaushikb@gmail.com>
2025-05-03 21:45:01 +0530

    revised express.js

---
+++ 11. ExpressJS/custom-middleware.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+
+const requestTimestampLogger = (req, res, next) => {
+    const timeStamp = new Date().toISOString();
+
+    console.log(`${timeStamp} from ${req.method} to ${req.url}`);
+    next();
+};
+
+app.use(requestTimestampLogger);
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.listen(3000, () => {
+    console.log(`Server is running at port 3000`);
+});
---
+++ 11. ExpressJS/middleware.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+
+// define middleware function
+const myFirstMiddleware = (req, res, next) => {
+    console.log('this first middleware will run on every request');
+
+    next();
+}
+
+app.use(myFirstMiddleware);
+
+app.get('/', (req, res) => {
+    res.send('Home Page');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.listen(3000, () => {
+    console.log(`Server is running at port 3000`);
+});
