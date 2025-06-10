8bf4467a925a468a8f5b8fd3cee773ac314e1419
kaushik0010 <paykolikaushikb@gmail.com>
2025-06-11 01:12:02 +0530

    Practiced GraphQL with nodejs

---
+++ 19.NodeJS GraphQL/src/data/products.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: "1",
+        title: "Laptop",
+        category: "Electronics",
+        price: 990.50,
+        inStock: true
+    },
+    {
+        id: "2",
+        title: "Coffee",
+        category: "Food",
+        price: 9,
+        inStock: true
+    },
+    {
+        id: "3",
+        title: "Shoes",
+        category: "Sports",
+        price: 300,
+        inStock: false
+    },
+];
+
+module.exports = products;
---
+++ 19.NodeJS GraphQL/src/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const {ApolloServer} = require('@apollo/server');
+const {startStandaloneServer} = require('@apollo/server/standalone')
+const connectDB = require('./database/db');
+const typeDefs = require('./graphql/schema')
+const resolvers = require('./graphql/resolvers')
+
+async function startServer() {
+    await connectDB();
+
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers,
+    });
+
+    const {url} = await startStandaloneServer(server, {
+        listen: {port: process.env.PORT || 4000}
+    });
+
+    console.log(`Server ready at: ${url}`);
+}
+
+startServer();
